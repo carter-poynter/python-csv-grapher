668604d34a512ed639eaaad252fea51467dcc0d2
Carter Poynter <poynter8574@beltc.ctc.edu>
2023-06-15 14:34:55 -0700

    Axis Titles / Scaling
    - Added titles for the X (Stock Index) and Y (Days) axes
    
    - Lowered the base scaling from 30 to 25
    
    - Re-centered the entire chart
    
    - Updated README

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-## Python .CSV Grapher
+# Python .CSV Grapher
 
 This script charts and graphs data in a .csv using Python Turtle.
 
-# CURRENTLY CAN ONLY BE RUN IN VISUAL STUDIO CODE
+### CURRENTLY CAN ONLY BE RUN IN VISUAL STUDIO CODE / PYTHON IDLE SHELL
 
-stockIndex.csv to input custom data
+Edit stockIndex.csv to input custom data.
+
+Edit *scaleFactor* variable to change the scaling of the graph.
--- csv-grapher.py
+++ csv-grapher.py
@@ -7,7 +7,7 @@ def plot_data_points(file_path):
         # Read the CSV data
         data = list(csv.reader(file))
 
-        scaleFactor = 30
+        scaleFactor = 25
 
         # Initialize the turtle at the first data point
         first_row = data[0]
@@ -21,7 +21,7 @@ def plot_data_points(file_path):
         # Make axes
         turtle.pencolor('white')
         turtle.penup()
-        turtle.goto(-450, -100)
+        turtle.goto(-350, -100)
 
         num_rows = len(data)
 
@@ -40,7 +40,7 @@ def plot_data_points(file_path):
             turtle.forward(20)
             turtle.forward(10)
 
-        turtle.goto(-450, -100)
+        turtle.goto(-350, -100)
 
         for i in range(20):
             turtle.setheading(90)
@@ -58,13 +58,21 @@ def plot_data_points(file_path):
             turtle.write((i + 1) * 10, align="center")
             turtle.forward(-20)
 
+        # Write Y Axis Title
+        turtle.goto(-450, 75)
+        turtle.write("Stock Index")
+
+        # Write X Axis Title
+        turtle.goto(0, -150)
+        turtle.write("Days")
+
         # Iterate over the data and plot the points
         
         previous_x = int(first_row[0]) * scaleFactor
         previous_y = int(first_row[1]) * (scaleFactor / 15)
 
         turtle.penup()
-        turtle.goto(int(first_row[0]) * scaleFactor - 450, int(first_row[1]) * (scaleFactor / 15) - 100)
+        turtle.goto(int(first_row[0]) * scaleFactor - 350, int(first_row[1]) * (scaleFactor / 15) - 100)
         turtle.pendown()
 
         for row in data[1:]:
@@ -78,7 +86,7 @@ def plot_data_points(file_path):
             elif y < previous_y:
                 turtle.pencolor('red')
 
-            turtle.goto(x - 450, y - 100)
+            turtle.goto(x - 350, y - 100)
 
             previous_x = x
             previous_y = y
@@ -86,12 +94,12 @@ def plot_data_points(file_path):
         # Go back to the beginning and plot black data points
         turtle.pencolor('white')
         turtle.penup()
-        turtle.goto(int(first_row[0]) * scaleFactor - 450, int(first_row[1]) * (scaleFactor / 15) - 100)
+        turtle.goto(int(first_row[0]) * scaleFactor - 350, int(first_row[1]) * (scaleFactor / 15) - 100)
         turtle.dot(8)
         for row in data[1:]:
             x = int(row[0]) * scaleFactor
             y = int(row[1]) * (scaleFactor / 15)
-            turtle.goto(x - 450, y - 100)
+            turtle.goto(x - 350, y - 100)
             turtle.dot(8)
 
     turtle.exitonclick()
